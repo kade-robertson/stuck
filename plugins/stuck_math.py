acffe6a8f6e2aa4459dfc30b2d4ab04c7a8ea66b
kade-robertson <kade.robertson16@gmail.com>
2015-08-19 10:43:06 -0400

    Revert "Revert "join + sort work better w/ strings, super overloaded + and -""
    This reverts commit 8c64007cc6ddf8a93406587acbfeecceb06e0a8f.

--- plugins/stuck_math.py
+++ plugins/stuck_math.py
@@ -8,8 +8,27 @@ CREATOR = 'Kade Robertson'
 VERSION = '1.2'
 DESCR   = 'Basic math functions plugin for the Stuck programming language.'
 
-def add_(s): return s + [s.pop(-2)+s.pop()]
-def sub_(s): return s + [s.pop(-2)-s.pop()]
+def add_(s):
+    k = s.pop()
+    if type(k) is list:
+        return s + [sum(k)]
+    else:
+        return s + [s.pop()+k]
+def sub_(s):
+    k = s.pop()
+    if type(k) is list:
+        if type(s[-1]) is list:
+            v = s.pop()
+            o = []
+            for a,b in zip(v,k):
+                if b: o += [a]
+            return s + [o]
+        else:
+            d=0
+            while k: d-=k.pop(0)
+            return s + [d]
+    else:
+        return s + [s.pop()-k]
 def mul_(s): return s + [s.pop(-2)*s.pop()]
 def mod_(s): return s + [s.pop(-2)%s.pop()]
 def gth_(s): return s + [s.pop(-2)>s.pop()]
